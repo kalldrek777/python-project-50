01de8aa1419b328b90a3fbb22f0327bdd6d9f941
gamid <kalldrek@yandex.ru>
2023-06-30 13:09:12 +0300

    fix

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,13 +1,29 @@
 import argparse
 import json
+import yaml
 
 
 def main(*args):
     if args:
-        print(777)
-        first_file = json.load(open(args[0]))
-        second_file = json.load(open(args[1]))
-        return result(first_file, second_file)
+        if args[0].endswith('.json'):
+            first_file = json.load(open(args[0]))
+        else:
+            with open(args[0], 'r') as f:
+                first_file = yaml.safe_load(f)
+
+        if args[1].endswith('.json'):
+            second_file = json.load(open(args[1]))
+        else:
+            with open(args[1], 'r') as f:
+                second_file = yaml.safe_load(f)
+
+        if args[2] == "plain":
+            return result_format_flag(first_file, second_file)
+        elif args[2] == "json":
+            return json.dumps(result(first_file, second_file))
+        elif args[2] == "stylish":
+            parse_result = result(first_file, second_file)
+            return generate_diff(parse_result, replacer=' ', space_count=4, _lvl=1)
     else:
         return parse_args()
 
@@ -22,14 +38,28 @@ def parse_args():
     parser.add_argument("second_file")
 
     # optional arguments
-    parser.add_argument("-f", "--format", help="set format of output")
+    parser.add_argument("-f", "--format", help="set format of output", default="stylish")
 
     args = parser.parse_args()
-    first_file = json.load(open(args.first_file))
-    second_file = json.load(open(args.second_file))
-    parse_result = result(first_file, second_file)
+    if args.first_file.endswith('.json'):
+        first_file = json.load(open(args.first_file))
+    else:
+        with open(args.first_file, 'r') as f:
+            first_file = yaml.safe_load(f)
 
-    return parse_result
+    if args.second_file.endswith('.json'):
+        second_file = json.load(open(args.second_file))
+    else:
+        with open(args.second_file, 'r') as f:
+            second_file = yaml.safe_load(f)
+    if args.format == "stylish":
+        parse_result = result(first_file, second_file)
+        return generate_diff(parse_result, replacer=' ', space_count=4, _lvl=1)
+    elif args.format == "plain":
+        return result_format_flag(first_file, second_file)[:-2]
+    elif args.format == "json":
+        return json.dumps(result(first_file, second_file))
+        # return result(first_file, second_file)
 
 
 def result(first_file, second_file):
@@ -45,31 +75,89 @@ def result(first_file, second_file):
     key_list = list(key_list_set)
     key_list.sort()
 
-    result = "{\n"
+    updated_dict = {}
 
     for key in key_list:
         if key in first_file:
             if key in second_file:
-                if first_file[key] == second_file[key]:
-                    result += "    " + key + ": " + str(first_file[key]) + "\n"
-                    print(key + ": " + str(first_file[key]))
-                elif first_file[key] != second_file[key]:
-                    result += "  " + '- ' + key + ": " + str(
-                        first_file[key]) + "\n" + "  " + '+ ' + \
-                              key + ": " + str(
-                        second_file[key]) + "\n"
-                    print('- ' + key + ": " + str(first_file[key]))
-                    print('+ ' + key + ": " + str(second_file[key]))
+                if isinstance(first_file[key], dict) and isinstance(second_file[key], dict):
+                    a = result(first_file[key], second_file[key])
+                    updated_dict[key] = a
+                else:
+                    if first_file[key] == second_file[key]:
+                        updated_dict[key] = first_file[key]
+                    elif first_file[key] != second_file[key]:
+                        updated_dict['- ' + key] = first_file[key]
+                        updated_dict['+ ' + key] = second_file[key]
             else:
-                result += "  " + '- ' + key + ": " + \
-                          str(first_file[key]) + "\n"
-                print('- ' + key + ": " + str(first_file[key]))
+                updated_dict['- ' + key] = first_file[key]
         else:
-            result += "  " + '+ ' + key + ": " + str(second_file[key]) + "\n"
-            print('+ ' + key + ": " + str(second_file[key]))
+            updated_dict['+ ' + key] = second_file[key]
 
-    result += "}\n"
-    print(result)
+    return updated_dict
+
+
+def result_format_flag(first_file, second_file, path=None):
+    if path is None:
+        path = []
+    key_list = []
+
+    for key, value in first_file.items():
+        key_list.append(key)
+
+    for key, value in second_file.items():
+        key_list.append(key)
+
+    key_list_set = set(key_list)
+    key_list = list(key_list_set)
+    key_list.sort()
+
+    result_str = ""
+
+    for key in key_list:
+        newpath = path + [key]
+        if key in first_file:
+            if key in second_file:
+                if isinstance(first_file[key], dict) and isinstance(second_file[key], dict):
+                    a = result_format_flag(first_file[key], second_file[key], newpath)
+                    result_str += a
+                else:
+                    if first_file[key] != second_file[key]:
+                        key_path = '.'.join(newpath)
+                        if isinstance(first_file[key], dict):
+                            result_str += f"Property '{key_path}' was updated. From [complex value] to '{second_file[key]}'\n"
+                        elif isinstance(second_file[key], dict):
+                            result_str += f"Property '{key_path}' was updated. From '{first_file[key]}' to [complex value]\n"
+                        else:
+                            result_str += f"Property '{key_path}' was updated. From '{first_file[key]}' to '{second_file[key]}'\n"
+
+
+            else:
+                key_path = '.'.join(newpath)
+                result_str += f"Property '{key_path}' was removed\n"
+        else:
+            key_path = '.'.join(newpath)
+            if isinstance(second_file[key], dict):
+                result_str += f"Property '{key_path}' was added with value: [complex value]\n"
+            else:
+                result_str += f"Property '{key_path}' was added with value: '{second_file[key]}'\n"
+    return result_str
+
+
+def generate_diff(value, replacer=' ', space_count=1, _lvl=1):
+    if isinstance(value, dict):
+        result = '{\n'
+        for el, val in value.items():
+            if el[:1] == "+" or el[:1] == "-":
+                result += f'{(replacer * space_count * _lvl)[2:]}{el}:'
+                if val != '':
+                    result += ' '
+            else:
+                result += f'{replacer * space_count * _lvl}{el}: '  # key to result str
+            result += generate_diff(val, replacer, space_count, _lvl + 1) + '\n'  # value to result str
+        result += replacer * space_count * (_lvl - 1) + '}'
+    else:
+        result = str(value)
     return result
 
 
--- tests/test_json.py
+++ tests/test_json.py
@@ -1,18 +1,144 @@
 import pytest
+import json
 
 from gendiff.scripts.gendiff import main
-import os, sys
-
-# result = os.system('poetry run gendiff file1.json file2.json')
-# print(result)
 
 
 @pytest.fixture
-def result():
-    result = main('file1.json', 'file2.json')
-    return result
+def diff_json_files():
+    result_stylish = main('file1.json', 'file2.json', 'stylish')
+    result_plain = main('file1.json', 'file2.json', 'plain')
+    result_json = main('file1.json', 'file2.json', 'json')
+    return result_stylish, result_plain, result_json
 
 
-def test_files(result):
-    assert result == "{\n  - follow: False\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n\
-  + timeout: 20\n  + verbose: True\n}\n"
+@pytest.fixture
+def diff_yaml_files():
+    result_stylish = main('filepath1.yml', 'filepath2.yml', 'stylish')
+    result_plain = main('filepath1.yml', 'filepath2.yml', 'plain')
+    result_json = main('filepath1.yml', 'filepath2.yml', 'json')
+    return result_stylish, result_plain, result_json
+
+
+def test_json(diff_json_files):
+    a, b, c = diff_json_files
+    assert a == ('{\n'
+                               '    common: {\n'
+                               '      + follow: False\n'
+                               '        setting1: Value 1\n'
+                               '      - setting2: 200\n'
+                               '      - setting3: True\n'
+                               '      + setting3: None\n'
+                               '      + setting4: blah blah\n'
+                               '      + setting5: {\n'
+                               '            key5: value5\n'
+                               '        }\n'
+                               '        setting6: {\n'
+                               '            doge: {\n'
+                               '              - wow:\n'
+                               '              + wow: so much\n'
+                               '            }\n'
+                               '            key: value\n'
+                               '          + ops: vops\n'
+                               '        }\n'
+                               '    }\n'
+                               '    group1: {\n'
+                               '      - baz: bas\n'
+                               '      + baz: bars\n'
+                               '        foo: bar\n'
+                               '      - nest: {\n'
+                               '            key: value\n'
+                               '        }\n'
+                               '      + nest: str\n'
+                               '    }\n'
+                               '  - group2: {\n'
+                               '        abc: 12345\n'
+                               '        deep: {\n'
+                               '            id: 45\n'
+                               '        }\n'
+                               '    }\n'
+                               '  + group3: {\n'
+                               '        deep: {\n'
+                               '            id: {\n'
+                               '                number: 45\n'
+                               '            }\n'
+                               '        }\n'
+                               '        fee: 100500\n'
+                               '    }\n'
+                               '}')
+    assert b == ("Property 'common.follow' was added with value: 'False'\n"
+                 "Property 'common.setting2' was removed\n"
+                 "Property 'common.setting3' was updated. From 'True' to 'None'\n"
+                 "Property 'common.setting4' was added with value: 'blah blah'\n"
+                 "Property 'common.setting5' was added with value: [complex value]\n"
+                 "Property 'common.setting6.doge.wow' was updated. From '' to 'so much'\n"
+                 "Property 'common.setting6.ops' was added with value: 'vops'\n"
+                 "Property 'group1.baz' was updated. From 'bas' to 'bars'\n"
+                 "Property 'group1.nest' was updated. From [complex value] to 'str'\n"
+                 "Property 'group2' was removed\n"
+                 "Property 'group3' was added with value: [complex value]\n")
+    assert json.loads(c) == json.load(open('test_format_json.json'))
+
+
+def test_yaml(diff_yaml_files):
+    a, b, c = diff_yaml_files
+    assert a == ('{\n'
+                               '    common: {\n'
+                               '      + follow: False\n'
+                               '        setting1: Value 1\n'
+                               '      - setting2: 200\n'
+                               '      - setting3: True\n'
+                               '      + setting3: None\n'
+                               '      + setting4: blah blah\n'
+                               '      + setting5: {\n'
+                               '            key5: value5\n'
+                               '        }\n'
+                               '        setting6: {\n'
+                               '            doge: {\n'
+                               '              - wow:\n'
+                               '              + wow: so much\n'
+                               '            }\n'
+                               '            key: value\n'
+                               '          + ops: vops\n'
+                               '        }\n'
+                               '    }\n'
+                               '    group1: {\n'
+                               '      - baz: bas\n'
+                               '      + baz: bars\n'
+                               '        foo: bar\n'
+                               '      - nest: {\n'
+                               '            key: value\n'
+                               '        }\n'
+                               '      + nest: str\n'
+                               '    }\n'
+                               '  - group2: {\n'
+                               '        abc: 12345\n'
+                               '        deep: {\n'
+                               '            id: 45\n'
+                               '        }\n'
+                               '    }\n'
+                               '  + group3: {\n'
+                               '        deep: {\n'
+                               '            id: {\n'
+                               '                number: 45\n'
+                               '            }\n'
+                               '        }\n'
+                               '        fee: 100500\n'
+                               '    }\n'
+                               '}')
+    assert b == ("Property 'common.follow' was added with value: 'False'\n"
+                 "Property 'common.setting2' was removed\n"
+                 "Property 'common.setting3' was updated. From 'True' to 'None'\n"
+                 "Property 'common.setting4' was added with value: 'blah blah'\n"
+                 "Property 'common.setting5' was added with value: [complex value]\n"
+                 "Property 'common.setting6.doge.wow' was updated. From '' to 'so much'\n"
+                 "Property 'common.setting6.ops' was added with value: 'vops'\n"
+                 "Property 'group1.baz' was updated. From 'bas' to 'bars'\n"
+                 "Property 'group1.nest' was updated. From [complex value] to 'str'\n"
+                 "Property 'group2' was removed\n"
+                 "Property 'group3' was added with value: [complex value]\n")
+    assert json.loads(c) == json.load(open('test_format_json.json'))
+
+
+
+
