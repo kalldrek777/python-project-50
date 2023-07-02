a3af51a59c26c1041ecf459f66a9351f1b031ede
gamid <kalldrek@yandex.ru>
2023-07-03 01:26:36 +0300

    fix

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,23 +1,5 @@
-import pytest
 import json
-
-from gendiff.scripts.gendiff import generate_diff
-
-
-@pytest.fixture
-def diff_json_files():
-    result_stylish = generate_diff('file1.json', 'file2.json', 'stylish')
-    result_plain = generate_diff('file1.json', 'file2.json', 'plain')
-    result_json = generate_diff('file1.json', 'file2.json', 'json')
-    return result_stylish, result_plain, result_json
-
-
-@pytest.fixture
-def diff_yaml_files():
-    result_stylish = generate_diff('filepath1.yml', 'filepath2.yml', 'stylish')
-    result_plain = generate_diff('filepath1.yml', 'filepath2.yml', 'plain')
-    result_json = generate_diff('filepath1.yml', 'filepath2.yml', 'json')
-    return result_stylish, result_plain, result_json
+from tests.fixtures import diff_json_files, diff_yaml_files
 
 
 def test_json(diff_json_files):
