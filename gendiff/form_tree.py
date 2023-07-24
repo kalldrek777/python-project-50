d7cffd12be3314ddcef2c1965c1b81105be31235
gamid <kalldrek@yandex.ru>
2023-07-24 17:17:54 +0300

    fix

--- gendiff/form_tree.py
+++ gendiff/form_tree.py
@@ -21,41 +21,40 @@ def diff(first_file, second_file, format_name='stylish', path=None):
                     a = diff(first_file[key],
                              second_file[key], format_name)
                     result.append({
-                                'key': key,
-                                'type': 'saved',
-                                'value': a
-                                })
+                        'key': key,
+                        'type': 'saved',
+                        'value': a
+                    })
                     # updated_dict[key] = a
                 else:
                     if first_file[key] == second_file[key]:
                         result.append({
-                                  'key': key,
-                                  'type': 'saved',
-                                  'value': first_file[key]
-                                })
+                            'key': key,
+                            'type': 'saved',
+                            'value': first_file[key]
+                        })
                         # updated_dict[key] = first_file[key]
                     elif first_file[key] != second_file[key]:
                         result.append({
-                                  'key': key,
-                                  'type': 'updated',
-                                  'value1': first_file[key],
-                                  'value2': second_file[key]
-                                })
+                            'key': key,
+                            'type': 'updated',
+                            'value1': first_file[key],
+                            'value2': second_file[key]
+                        })
                         # updated_dict['- ' + key] = first_file[key]
                         # updated_dict['+ ' + key] = second_file[key]
             else:
                 result.append({
-                                  'key': key,
-                                  'type': 'deleted',
-                                  'value': first_file[key]
-                                })
+                    'key': key,
+                    'type': 'deleted',
+                    'value': first_file[key]
+                })
                 # updated_dict['- ' + key] = first_file[key]
         else:
             result.append({
-                                  'key': key,
-                                  'type': 'added',
-                                  'value': second_file[key]
-                                })
+                'key': key,
+                'type': 'added',
+                'value': second_file[key]
+            })
             # updated_dict['+ ' + key] = second_file[key]
-    return (result)
     return result
